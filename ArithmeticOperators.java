9313fe21f158d08a605ad8d3a767709d73d6ca10
HSKatyayini <hskatyayini@gmail.com>
2025-09-15 21:29:48 +0530

    Add files via upload

---
+++ ArithmeticOperators.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package arithmeticoperators;
+import java.lang.*;
+public class ArithmeticOperators {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=10;
+        int b=5;
+        System.out.println("a+b="+(a+b));
+        System.out.println("a-b="+(a-b));
+        System.out.println("a*b="+(a*b));
+        System.out.println("a/b="+(a/b));
+        System.out.println("a%b="+(a%b));
+        System.out.println("Storing the values to another variables");
+         int c=a+b;
+         int d=a-b;
+         int e=a*b;
+         int f=a/b;
+         int g=a%b;
+         System.out.println(c);
+         System.out.println(d);
+         System.out.println(e);
+         System.out.println(f);
+         System.out.println(g);
+    }
+}
+    
+    
+
