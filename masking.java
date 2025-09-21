18d1f01d0fc9af6da7d0b77b7cf05c1940b8316f
HSKatyayini <hskatyayini@gmail.com>
2025-09-21 21:24:01 +0530

    Add files via upload

---
+++ masking.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bitwiseopearator;
+public class masking {
+    public static void main(String args[]){
+        /*Masking process-means to check the bit in number is on or off by 
+        using AND opearator*/
+        int a=72;//0101000
+        int b=8;//0001000
+        System.out.println(a&b);//0001000
+    }
+}
